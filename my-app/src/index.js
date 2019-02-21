858e59035c046d56943e63a8ce548a2ea02faacf
Rishav Medhi <rishav@chalkstreet.com>
2019-02-21 18:50:59 +0530

    Update index.js
    using variables/expressions in JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+const name = 'Rishav Medhi';
+const element = <h1>Hello, {name}</h1>;
+
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    element,
     document.getElementById('root')
 );
