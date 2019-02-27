ba2cae60362614f0dc76a4689cd5e2ad45ebb08a
Rishav Medhi <rishav@chalkstreet.com>
2019-02-27 20:31:06 +0530

    Using attributes in JSX
    Using attributes both string values and values derived from variables

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,10 +6,12 @@ function formatname(name){
     return name.first_name+' '+name.last_name;
 }
 
+// using JSX as expressions
 function conditionFunction(name)
 {
+    // using attributes in JSX
     if(name)
-        return <h1>True conditon by {formatname(name)}</h1>;
+        return <h1 data-val={name.first_name} data-type="name">True conditon by {formatname(name)}</h1>;
     return <h1>Didn't get any name</h1>;
 }
 
