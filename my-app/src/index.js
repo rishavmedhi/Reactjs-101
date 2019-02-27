3e28a9bb298de329a42be3f22332ebdae286a46d
Rishav Medhi <rishav@chalkstreet.com>
2019-02-27 20:20:24 +0530

    using function in JSX
    Creating a function that combines key values inside a variable by using a function and displaying it using JSX

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const name = 'Rishav Medhi';
-const element = <h1>Hello, {name}</h1>;
+function formatname(name){
+    return name.first_name+' '+name.last_name;
+}
+
+const name =
+    {
+        'first_name': 'Rishav',
+        'last_name' : 'Medhi'
+    };
+const element = <h1>Hello, {formatname(name)}</h1>;
 
 ReactDOM.render(
     element,
