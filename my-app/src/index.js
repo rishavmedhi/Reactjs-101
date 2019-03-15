4a98bfaa10a04a48809c4f0961fd202a4dbaf35e
Rishav Medhi <rishav164@gmail.com>
2019-03-15 14:55:49 +0530

    React State : Component Function to Class
    Starting with states, converting a component timer function to class

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -88,7 +88,24 @@ const user_props = {
     "description":"Coding in react"
 }
 
+/* Working with states
+    Starting with converting a component function to class 
+*/
+class Clock extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+            </div>
+        )
+    }
+}
+
+setInterval(update_timer,10);
+
 ReactDOM.render(
-    intro_element(user_props),
+    <Clock date={new Date()} />,
     document.getElementById('root')
 );
+
