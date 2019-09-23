2c45145507198dccba75243e30c30c02e8f8b55f
Rishav Medhi <rishav164@gmail.com>
2019-09-23 12:47:01 +0530

    Starting with 'Lifting Up States'
    In the TemperatureInput component, we are changing the local states to prop which will be sent by the Calculator component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -641,12 +641,14 @@ class TemperatureInput extends React.Component{
 
     handleChange(e)
     {
-        this.setState({temperature : e.target.value});
+        // this.setState({temperature : e.target.value});
+        this.props.onTemperatureChange(e.target.value);
     }
 
     render()
     {
-        const temperature = this.state.temperature;
+        // const temperature = this.state.temperature;
+        const temperature  = this.props.temperature;
         const scale = this.props.scale;
 
         return(
