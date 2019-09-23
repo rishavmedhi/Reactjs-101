2e7ad59f9dc61deab031d22063dee77280edee8a
Rishav Medhi <rishav164@gmail.com>
2019-09-23 12:20:34 +0530

    UI for temperature in 2 units
    UI for temperature in fahrenheit and Celcius. Calculator component calls the Temperature inout and passes parameter to work for that specific unit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -619,19 +619,63 @@ class Calculator extends React.Component
     {
         const temperature = this.state.temperature;
         return (
+            <div>
+                <TemperatureInput scale='c' />
+                <TemperatureInput scale='f' />
+            </div>
+        )
+    }
+}
+
+const scaleNames = {
+    c: 'Celcius',
+    f: 'Fahrenheit'
+};
+
+class TemperatureInput extends React.Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature : ''};
+    }
+
+    handleChange(e)
+    {
+        this.setState({temperature : e.target.value});
+    }
+
+    render()
+    {
+        const temperature = this.state.temperature;
+        const scale = this.props.scale;
+
+        return(
             <fieldset>
-                <legend>Enter the temperature in Celcius</legend>
-                <input
-                    value={temperature}
-                    onChange={this.handleChange}
-                    type="number"
-                />
-                <BoilingVerdict
-                    celcius={parseFloat(temperature)}
-                />
+                <legend>Enter the temperature in {scaleNames[scale]}</legend>
+                <input value={temperature}
+                        onChange={this.handleChange} />
             </fieldset>
         )
     }
+}
+
+function toCelcius(fahrenheit){
+    return (fahrenheit-32)*5/9;
+}
+
+function toFahrenheit(celcius) {
+    return (celcius-9/5) + 32;
+}
+
+function tryConvert(temperature, convert){
+    const input = parseFloat(temperature);
+    if(Number.isNaN(input)){
+        return '';
+    }
+
+    const output = convert(input);
+    const rounded = Math.round(output*1000) / 1000;
+    return rounded.toString();
 }
 
 /* using conditional operator */
