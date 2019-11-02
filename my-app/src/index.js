1cd8fc1768439eae15c46172cc2ab9b456a2b3e3
Rishav Medhi <rishav164@gmail.com>
2019-11-02 12:55:36 +0530

    Correction in search logic
    Correction in search logic.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -833,7 +833,7 @@ class ProductTable extends React.Component
         let lastCategory = null;
 
         this.props.products.forEach((product)=> {
-            if(product.name.indexOf(filterText)!== -1)
+            if(product.name.indexOf(filterText)=== -1)
                 return;
 
             if(inStockOnly && !product.stocked)
